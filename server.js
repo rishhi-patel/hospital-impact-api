47204549b11bb87ccce2a902d0dc0a492427eb29
Rishhi <98315242+rishhi-patel@users.noreply.github.com>
2025-03-14 19:53:37 -0400

    - -added randomized response

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+import express from "express"
+import bodyParser from "body-parser"
 
 const app = express()
 const PORT = 3000
 
 // Enable CORS
-app.use((req, res, next) => {
+app.use((_, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
   res.header(
@@ -18,65 +18,54 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 
 app.post("/calculate-impact", (req, res) => {
+  const { services } = req.body.parameters || []
+
+  const generateRandomData = (caseVolume) => {
+    const potentialCaseVolume = caseVolume + Math.floor(Math.random() * 100)
+    const blocks = Math.floor(Math.random() * 400)
+    const potentialBlocks = blocks - Math.floor(Math.random() * 20)
+    const potentialCostSaved = parseFloat((Math.random() * 500000).toFixed(2))
+    const potentialByBucket = [
+      {
+        bucketName: "Planning Accuracy",
+        volumeIncreased: Math.floor(Math.random() * 20),
+        blocksReduced: Math.floor(Math.random() * 5),
+        costSaved: parseFloat((Math.random() * 100000).toFixed(2)),
+      },
+      {
+        bucketName: "Flow Smoothing",
+        volumeIncreased: Math.floor(Math.random() * 20),
+        blocksReduced: Math.floor(Math.random() * 5),
+        costSaved: parseFloat((Math.random() * 100000).toFixed(2)),
+      },
+      {
+        bucketName: "Priority Planning",
+        volumeIncreased: Math.floor(Math.random() * 60),
+        blocksReduced: Math.floor(Math.random() * 15),
+        costSaved: parseFloat((Math.random() * 300000).toFixed(2)),
+      },
+    ]
+
+    return {
+      caseVolume,
+      potentialCaseVolume,
+      blocks,
+      potentialBlocks,
+      potentialCostSaved,
+      potentialByBucket,
+    }
+  }
+
+  const data = {}
+  services &&
+    services.forEach((service) => {
+      data[service.serviceName] = generateRandomData(service.caseVolume)
+    })
+
   const response = {
     isSuccess: true,
     message: "success",
-    data: {
-      Cardiac: {
-        caseVolume: 1800,
-        potentialCaseVolume: 1881,
-        blocks: 356,
-        potentialBlocks: 341,
-        potentialCostSaved: 234273.54,
-        potentialByBucket: [
-          {
-            bucketName: "Planning Accuracy",
-            volumeIncreased: 11,
-            blocksReduced: 2,
-            costSaved: 32440.65,
-          },
-          {
-            bucketName: "Flow Smoothing",
-            volumeIncreased: 15,
-            blocksReduced: 3,
-            costSaved: 42218.86,
-          },
-          {
-            bucketName: "Priority Planning",
-            volumeIncreased: 55,
-            blocksReduced: 11,
-            costSaved: 159614.02,
-          },
-        ],
-      },
-      General: {
-        caseVolume: 1500,
-        potentialCaseVolume: 1573,
-        blocks: 328,
-        potentialBlocks: 312,
-        potentialCostSaved: 242883.89,
-        potentialByBucket: [
-          {
-            bucketName: "Planning Accuracy",
-            volumeIncreased: 6,
-            blocksReduced: 1,
-            costSaved: 18775.57,
-          },
-          {
-            bucketName: "Flow Smoothing",
-            volumeIncreased: 20,
-            blocksReduced: 4,
-            costSaved: 67702.05,
-          },
-          {
-            bucketName: "Priority Planning",
-            volumeIncreased: 47,
-            blocksReduced: 10,
-            costSaved: 156406.27,
-          },
-        ],
-      },
-    },
+    data,
   }
 
   res.json(response)
